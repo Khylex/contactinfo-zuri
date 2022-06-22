bf57249680d3c0d0c16f385637c8eb04dcbb5a7c
khylex <geoang222@gmail.com>
2022-06-22 18:02:15 +0100

    feat: new submit

---
+++ index.js
@@ -0,0 +1,9 @@
+const person = {
+    name: "George Angel",
+    height: 177.76,
+    country: "Nigeria",
+  };
+  
+  console.log(
+    `Hi, my name is ${person.name}, I'm from ${person.country} and my height is about ${person.height}cm. Nice to meet you.`
+  );
